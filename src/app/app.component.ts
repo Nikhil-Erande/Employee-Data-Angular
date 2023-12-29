37cee34fa259f8dc8fcd940b60b8822b447cb1d0
Nikhil <nikhilerande14@outlook.com>
2023-12-29 08:47:54 +0530

    Your commit message here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import EmployeeJson from './employee.json';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 interface EMPLOYEE {
   id: number;
@@ -13,21 +17,17 @@ interface EMPLOYEE {
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule,MatTableModule,MatTableModule,
+    MatIconModule,
+    MatSortModule,
+    MatPaginatorModule,],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  Employees: EMPLOYEE[] = EmployeeJson;
+  Users: EMPLOYEE[] = EmployeeJson;
 
   constructor() {
-    console.log(this.Employees);
+    console.log(this.Users);
   }
 }
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
